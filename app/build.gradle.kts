eb158143eb68a1a6e75b32db2f802e3381dd84b6
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2025-09-05 11:49:04 -0600

    Firebase implementado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-}
+    // Removed: id("com.android.application")
 
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+}
 android {
     namespace = "com.narmocorp.satoriospa"
     compileSdk = 36
@@ -56,4 +59,15 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
